0f2302744f9c9bad6d4f94d7cf49da4d14ec2696
phillipho98 <phillip.ho98@gmail.com>
2026-01-05 14:23:48 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+/**
+ * AutoBrand CRM Authentication Configuration
+ * Generated for testing purposes
+ * 
+ * ⚠️  DO NOT COMMIT THIS FILE TO VERSION CONTROL
+ * 
+ * Test Credentials:
+ *   Username: admin
+ *   Password: AutoBrand2026!
+ * 
+ * To change credentials, use generate-password.html
+ */
+
+const AUTH_CONFIG = {
+    // admin (lowercase)
+    usernameHash: '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918',
+    // AutoBrand2026!
+    passwordHash: '4fa3ad237f018e031a48c3ea2b1561cd1872b572eab3f7876a05853e9e7f0d4a'
+};
